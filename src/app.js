c2811b4f49942ce4d830fec4dc9ad338214f72ec
Vu Nguyen <khacvu0505@gmail.com>
2024-05-16 12:43:42 +0700

    Create folder and init some packages

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+// Use morgan to log requests to the console
+const morgan = require("morgan");
+// Use compression to compress responses
+const compression = require("compression");
+// Use helmet to secure Express apps by setting various HTTP headers
+const helmet = require("helmet");
+
+// Content
+const app = express();
+
+// init middlewares
+app.use(morgan("dev")); // "dev" | "common" | "short" | "tiny" | "combined
+app.use(helmet());
+app.use(compression());
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "HEllo from tips js";
+  return res.status(200).json({
+    message: "Hello World",
+    metadata: strCompress.repeat(10000),
+  });
+});
+
+// handling errors
+
+module.exports = app;
